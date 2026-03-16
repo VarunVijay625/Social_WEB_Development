68259b50f9cc10f1ef5ca0c7d3d83c56b0b5a806
Varun Vijay <vvijay@uvm.edu>
2026-03-16 16:05:31 -0400

    edited Freds name

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-        apiKey: "AIzaSyDP7snJVxhiQSQs7Z8CzGA9uvk5qdLkJhQ",
-        authDomain: "social-web-dev.firebaseapp.com",
-        projectId: "social-web-dev",
-        storageBucket: "social-web-dev.firebasestorage.app",
-        messagingSenderId: "621332128804",
-        appId: "1:621332128804:web:94424f8ba5b97549fbe4b1",
-        measurementId: "G-S0J91M5KEQ"
+    apiKey: "AIzaSyDP7snJVxhiQSQs7Z8CzGA9uvk5qdLkJhQ",
+    authDomain: "social-web-dev.firebaseapp.com",
+    projectId: "social-web-dev",
+    storageBucket: "social-web-dev.firebasestorage.app",
+    messagingSenderId: "621332128804",
+    appId: "1:621332128804:web:94424f8ba5b97549fbe4b1",
+    measurementId: "G-S0J91M5KEQ"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
@@ -154,7 +154,7 @@ async function do_query(id, column) {
 let people = [
     'Ellie G',
     'Jes B',
-    'Frederique',
+    'Frederique N',
     'James D',
     'Paige S',
     'Varun V.',
