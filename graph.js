42259e6f54e7302a585d694aeecc8074952a2b43
jbennett979 <jabennet@uvm.edu>
2026-04-07 22:02:29 -0400

    fixed login button issue

--- graph.js
+++ graph.js
@@ -46,6 +46,24 @@ async function loadGraph() {
     drawGraph(nodes, links);
 }
 
+function myFunction() {
+    document.getElementById("create-login").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.drop-btn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
 function drawGraph(nodes, links) {
     const width = window.innerWidth;
     const height = window.innerHeight;
